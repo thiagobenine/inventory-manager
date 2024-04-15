b1707934943de02399aa7466b60de8ff94b645ca
Thiago Benine <thiagobenine@gmail.com>
2024-04-14 21:36:11 -0300

    refactor: using ObjectId instead of uuid

--- src/domain/ports/outbound/repositories/order.py
+++ src/domain/ports/outbound/repositories/order.py
@@ -1,10 +1,11 @@
 from typing import Protocol
-from uuid import UUID
+
+from bson import ObjectId
 
 from src.domain.entities.order import Order
 
 
 class OrderRepositoryInterface(Protocol):
-    def find_order_by_id(self, order_id: UUID) -> Order: ...
+    def find_order_by_id(self, order_id: ObjectId) -> None | Order: ...
 
     def save(self, order: Order) -> None: ...
