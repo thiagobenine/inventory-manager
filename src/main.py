3fe8b1a6f117a318f5ad0077f0b5101efb7e700b
Thiago Benine <thiagobenine@gmail.com>
2024-06-06 02:37:54 -0300

    correlation

--- src/main.py
+++ src/main.py
@@ -11,12 +11,47 @@ from src.adapters.outbound.repositories.mongo.connection import (
 )
 from src.adapters.outbound.repositories.mongo.item import MongoItemRepository
 from src.adapters.outbound.repositories.mongo.order import MongoOrderRepository
-from myloglib.formatters.new_relic_correlation import Correlation
-if __name__ == "__main__":
-    logger = LoggerFactory.build_default_logger("test_logger")
-    Correlation.set_id("123")
 
-    logger.info("Starting application 5")
+import uuid
+from contextvars import ContextVar
+
+NO_CORRELATION = "NO_CORRELATION"
+
+class CorrelationEntity:
+    id: str
+
+    __slots__ = ("id",)
+
+    def __init__(self, value: str | None = "") -> None:
+        self.id = value or str(uuid.uuid4())
+
+
+_context: ContextVar[CorrelationEntity] = ContextVar(
+    "correlation_other", default=CorrelationEntity(NO_CORRELATION)
+)
+
+
+class Correlation:
+    HEADER_KEY = "X-Correlation-ID"
+
+    @classmethod
+    def get_id(cls) -> str:
+        return _context.get().id
+
+    @staticmethod
+    def set_id(value: str | None) -> None:
+        _context.set(CorrelationEntity(value))
+
+    @classmethod
+    def build_header(cls) -> dict[str, str]:
+        return {cls.HEADER_KEY: cls.get_id()}
+
+
+if __name__ == "__main__":
+    logger = LoggerFactory.build_default_logger("test_logger", Correlation)
+    Correlation.set_id("9999")
+
+    logger.info("Starting application 10")
     connection = MongoConnection(
         os.getenv("MONGO_CONNECTION_STRING"),
     )
